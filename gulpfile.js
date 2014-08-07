f58b05adf12bf8829ad21b0a43a6b11e44a16821
Miha-ha <miha-ha@yandex.ru>
2014-08-07 08:25:39 +0400

    Release v0.0.2

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+/**
+ * Created by Miha-ha on 01.08.14.
+ */
+var gulp = require('gulp'),
+    sequence = require('run-sequence'),
+    bump = require('gulp-bump'),
+    exec = require('gulp-exec'),
+    rimraf = require('gulp-rimraf'),
+    paths = {
+    };
+
+
+
+
+
+/**
+ * RELEASE
+ */
+
+gulp.task('rimraf', function () {
+    return gulp.src(['dist/*', 'release.zip'], { read: false })
+        .pipe(rimraf());
+});
+
+gulp.task('bump', function () {
+    return gulp.src(['./package.json'])
+        .pipe(bump())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('git', function () {
+    var v = 'v' + require('./package.json').version,
+        options = {
+            silent: true,
+            v: v,
+            msg: 'Release ' + v
+        };
+
+    return gulp.src('')
+        .pipe(exec('git commit -am "<%=options.msg%>"', options))
+        .pipe(exec('git tag -a <%=options.v%> -m "<%=options.msg%>"', options))
+        .pipe(exec('git push origin --follow-tags')); //отправляю изменения и тэги вместе
+//        .pipe(exec('git push origin --tags'));
+});
+
+gulp.task('zip', ['dist'], function () {
+    return gulp.src('').pipe(exec('cd dist && zip -r ../release.zip ./ && cd ..', {
+        silent: true,
+        continueOnError: true
+    }));
+});
+
+gulp.task('deploy-test', function () {
+    var v = require('./package.json').version;
+
+    return gulp.src('').pipe(
+        exec('ssh deploy@192.168.10.10 site-v2 test v' + v, {
+            silent: false,
+            continueOnError: false
+        }));
+});
+
+gulp.task('deploy-release', function () {
+    var v = require('./package.json').version;
+
+    return gulp.src('').pipe(
+        exec('ssh deploy@192.168.10.10 site-v2 deploy v' + v, {
+            silent: false,
+            continueOnError: false
+        }));
+});
+
+gulp.task('log', function () {
+    return gulp.src('').pipe(
+        exec('ssh deploy@192.168.10.10 tail site-v2.log', {silent: false})
+    );
+});
+
+gulp.task('github-release', require('./github/release').createAndUpload);
+
+//combo tasks
+
+gulp.task('release', function (cb) {
+    sequence(
+        'bump',
+        'git',
+        'zip',
+        'github-release',
+//        'deploy-test',
+//        'deploy-release',
+//        'rimraf',
+        cb);
+});
